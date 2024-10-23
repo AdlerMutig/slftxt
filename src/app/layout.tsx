f46730934aea43839bfd4d4745b5b21d1fde52a5
Paolo <paoloagva@gmail.com>
2024-10-23 17:57:33 +0200

    Add footer and navbar
    Basic navigation with Link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +19,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
     return (
         <html lang="en">
             <body className={inter.className}>
-                <h1>This is the main layout</h1>
+                <Navbar />
                 {children}
+                <Footer />
             </body>
         </html>
     );
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import Links from "./links/Links";
+
+const Navbar = () => {
+    return (
+        <div>
+            <div>Loggo</div>
+            <div>
+                <Links />
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
